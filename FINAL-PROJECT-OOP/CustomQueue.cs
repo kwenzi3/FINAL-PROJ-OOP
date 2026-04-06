53b6725b859b80ad565b3ce9fd9838f0b9adb21e
unknown <enicnarffernandez@gmail.com>
2026-04-06 15:46:04 -0400

    Custom Queque class

---
+++ FINAL-PROJECT-OOP/CustomQueue.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FINAL_PROJECT_OOP
+{
+    public class CustomQueue<T>
+    {
+        private T[] Qaaray;
+        private int count;
+
+        public CustomQueue(int capacity)
+        {
+            if (count <= 0) 
+                throw new EmptyStructureException("Capacity must be greater than zero.");
+
+            Qaaray = new T[capacity];
+            count = 0;
+        }
+
+        public int Countt { get { return count; } }
+
+        public void Enqueue(T item)
+        {
+            if (count >= Qaaray.Length)
+                throw new OverCapacityException("Queue is at full capacity.");
+            Qaaray[count] = item;
+            count++;
+        }
+
+        public T Dequeque()
+        {
+            if (count == 0)
+                throw new EmptyStructureException("Queue is empty.");
+
+            T item = Qaaray[0];
+            for (int i = 1; i < count; i++)
+            {
+                Qaaray[i - 1] = Qaaray[i];
+            }
+            count--;
+            return item;
+        }
+
+        public T Peek()
+        {
+            if (count == 0)
+                throw new EmptyStructureException("Queque is empty. Cannot peek");
+            return Qaaray[0];
+            
+        }
+
+        public bool IsEmpty()
+        {
+            return count == 0;
+        }
+         public bool IsEmpty(T item)
+         {
+            if(Qaaray.Length == 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+
+         }
+
+
+
+
+
+    }
+}
