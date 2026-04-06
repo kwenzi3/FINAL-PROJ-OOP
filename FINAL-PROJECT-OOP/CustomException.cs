c3b2a2bfd168c67364a9e4b1e9e863a0767407b9
unknown <enicnarffernandez@gmail.com>
2026-04-06 15:32:32 -0400

    Exception class

---
+++ FINAL-PROJECT-OOP/CustomException.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FINAL_PROJECT_OOP
+{
+   public class InvalidDataException : Exception
+   {
+        public InvalidDataException() : base() { }
+
+        public InvalidDataException(string message) : base(message) { }
+       
+   }
+    public class OverCapacityException : Exception
+    {
+        public OverCapacityException() : base() { }
+
+        public OverCapacityException(string message) : base(message) { }
+
+    }
+    public class EmptyStructureException : Exception
+    {
+        public EmptyStructureException() : base() { }
+
+        public EmptyStructureException(string message) : base(message) { }
+
+    }
+}
