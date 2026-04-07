ecdc25a6b5607afe429be29cbc01220311e55c97
kasunya agampodi <kasu.agam@gmail.com>
2026-04-07 02:07:18 -0400

    package

--- FINAL-PROJECT-OOP/Program.cs
+++ FINAL-PROJECT-OOP/Program.cs
@@ -10,8 +10,18 @@ namespace FINAL_PROJECT_OOP
     {
         static void Main(string[] args)
         {
-           
+            Console.WriteLine("1. Add a Vehicle!");
+            Console.WriteLine("1. Add a Worker!");
+            Console.WriteLine("1. Add a Package!");
+            Console.WriteLine("2. Assign Delivery/Deliveries");
+            Console.WriteLine("3. Sort");
+            Console.WriteLine("4. Search");
+            Console.WriteLine("5. Run stimulation");
+            Console.WriteLine("6. Undo");
+            Console.WriteLine("7. Save or Load");
 
+            Console.WriteLine("What is your option?");
+            int choice =int.Parse(Console.ReadLine());
         }
     }
 }
