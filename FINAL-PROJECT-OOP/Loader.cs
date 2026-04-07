1e425ce4705a46eb06df1c96d8fc7b74f78f81b7
unknown <enicnarffernandez@gmail.com>
2026-04-07 01:17:24 -0400

    Loader class

---
+++ FINAL-PROJECT-OOP/Loader.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FINAL_PROJECT_OOP
+{
+    internal class Loader : Worker
+    {
+        private double Maxliftweight;
+
+
+        public Loader() : base()
+        {
+            Maxliftweight = 0.0;
+        }
+
+        public Loader (int wi, string n, int ey, int tc, bool ia, string lt, double mw) :
+            base(wi, n, ey, tc, ia)
+        {
+            if (mw <= 0)
+                throw new EmptyStructureException("License type cannot be null or empty.");
+
+            Maxliftweight = mw;
+        }
+
+        public double GetMaxLiftWeight() { return Maxliftweight; }
+        
+        public void SetMaxliftweight(double mw) 
+        {
+
+            if (mw <= 0)
+                throw new EmptyStructureException("License type cannot be null or empty.");
+
+
+            Maxliftweight = mw; 
+        }
+
+
+        public override void PerformTask()
+        {
+            Console.WriteLine("Loading/unload packages");   
+        }
+    }
+}
