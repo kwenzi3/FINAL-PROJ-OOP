97727abdede9c59e003741d063887334ae94be6e
kasunya agampodi <kasu.agam@gmail.com>
2026-04-10 21:51:38 -0400

    package and ifilehandler

---
+++ FINAL-PROJECT-OOP/IFilehandler.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FINAL_PROJECT_OOP
+{
+    public interface IFilehandler
+    {
+        void save(string path);
+
+        void load(string path);
+
+    }
+}
